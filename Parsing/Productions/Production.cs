d4bfeeeb2825b445047ccb6ac6e2e5d14134b8bd
Lino Contreras <A01700457@itesm.mx>
2021-02-26 13:54:49 -0800

    Develop (#2)
    * Rename to match tokens
    
    * Add more actions
    
    * Actions up to state 50
    
    * Add actions

--- Parsing/Productions/Production.cs
+++ Parsing/Productions/Production.cs
@@ -3,8 +3,14 @@ namespace Parsing {
 
     public class Production : Symbol {
         public int Id { get; private set; }
+
         public Production(int number) : base(SymbolType.Production) {
             this.Id = number;
+        }
+
+        public override string ToString()
+        {
+            return $"[{nameof(SymbolType.Production)}] ({this.Id})";
         }
     }
 }
